8e4136f3062b57a767418cd3c7dab83d926f5ba2
abdelhadi <abdelhadi.lemzouri31@gmail.com>
2025-02-23 18:02:50 +0000

    first commit ilyas

--- demo/src/main/java/com/example/demo/entities/Employee.java
+++ demo/src/main/java/com/example/demo/entities/Employee.java
@@ -2,6 +2,10 @@ package com.example.demo.entities;
 
 public class Employee {
     private int id;
+    private String firstName;//ilyas
     private String name;
+    private String lastName; //ilyas
     private int age;
+    private String email; //ilyas
+    public Employee(int id, String name, int age, String email) {} //ilyas
 }
