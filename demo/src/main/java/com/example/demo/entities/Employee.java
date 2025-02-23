e0adfa5c68ad68b8cfe69309888a622f944870ad
abdelhadi <abdelhadi.lemzouri31@gmail.com>
2025-02-23 18:10:37 +0000

    firt commit abdelhadi

--- demo/src/main/java/com/example/demo/entities/Employee.java
+++ demo/src/main/java/com/example/demo/entities/Employee.java
@@ -2,10 +2,18 @@ package com.example.demo.entities;
 
 public class Employee {
     private int id;
-    private String firstName;//ilyas
-    private String name;
-    private String lastName; //ilyas
+
+
+
     private int age;
-    private String email; //ilyas
+
     public Employee(int id, String name, int age, String email) {} //ilyas
+
+
+    public Employee(int id, String firstName, String name, String lastName, int age, String email){
+        this.id = id;
+    }//abdelhadi
+
+
+
 }
--- demo/src/main/java/com/example/demo/entities/Student.java
+++ demo/src/main/java/com/example/demo/entities/Student.java
@@ -1,6 +1,18 @@
 package com.example.demo.entities;
 
 public class Student {
-    private int id; //ilyas
-    private String name; //ilyas
+    private int id;
+    private String name;
+    private int age; //abdelhadi
+    private String gender; //abdelhadi
+    private String email; //abdelhadi
+    private String phone;
+
+
+    public Student(int id, String name, int age, String gender, String email) {
+        this.id = id;
+    }//abdelhadi
+    public int getId() {
+        return id;
+    }//abdelhadi
 }
